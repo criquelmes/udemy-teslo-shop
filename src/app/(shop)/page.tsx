05d9e66b5960291d7e0f89634a9b824667fb40a5
Christopher Riquelme <ccriquelmes@gmail.com>
2025-06-01 20:26:38 -0400

    Componente - Title

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,9 @@
-import { titleFont } from "../../config/fonts";
+import { Title } from "@/components";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      <h1>hola</h1>
-      <h1 className={titleFont.className}>hola mundo</h1>
-    </div>
+    <>
+      <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
+    </>
   );
 }
