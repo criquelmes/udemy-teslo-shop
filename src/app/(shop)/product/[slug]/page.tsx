a2a2742af519925551c26bf6b4c2c71998189cf9
Christopher Riquelme <ccriquelmes@gmail.com>
2025-06-03 02:56:57 -0400

    Slideshow Desktop

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { QuantitySelector, SizeSelector } from "@/components";
+import { ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
 import { titleFont } from "@/config/fonts";
 import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
@@ -21,7 +21,7 @@ export default async function ProductPage({ params }: Props) {
     <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
       {/* Slideshow */}
       <div className="col-span-1 md:col-span-2 ">
-        <h1>Hola mundo</h1>
+        <ProductSlideshow images={product.images} title={product.title} />
       </div>
 
       {/* Detalles */}
