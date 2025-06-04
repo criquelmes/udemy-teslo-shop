3029c5f68742f8ae4a0ec9e95b6ace6df8bd40db
Christopher Riquelme <ccriquelmes@gmail.com>
2025-06-04 02:11:43 -0400

    Fix detalles esteticos carrito de compras

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -51,7 +51,7 @@ export default function CartPage() {
           {/* Items */}
 
           {/* Checkout */}
-          <div className="bg-white rounded-xl shadow-xl p-7">
+          <div className="bg-white rounded-xl shadow-xl p-7 h-fit">
             <h2 className="text-2xl mb-2">Checkout</h2>
             <div className="grid grid-cols-2">
               <span># Products</span>
