2d994de229119b19e7681b84c80c23afaec5485b
Christopher Riquelme <ccriquelmes@gmail.com>
2025-06-05 13:03:42 -0400

    Productos por genero

---
+++ src/app/(shop)/gender/[gender]/page.tsx
@@ -0,0 +1,50 @@
+// import { notFound } from "next/navigation";
+
+import { getPaginatedProductsWithImages } from "@/actions";
+import { Pagination, ProductGrid, Title } from "@/components";
+import { Gender } from "@/generated/prisma/client";
+import { initialData } from "@/seed/seed";
+import { redirect } from "next/navigation";
+
+interface Props {
+  params: {
+    gender: string;
+  };
+
+  searchParams?: {
+    page?: string;
+  };
+}
+
+const seedProducts = initialData.products;
+
+export default async function CategoryPage({ params, searchParams }: Props) {
+  const { gender } = await params;
+
+  const page = searchParams?.page ? parseInt(searchParams.page) : 1;
+  const { products, currentPage, totalPages } =
+    await getPaginatedProductsWithImages({ page, gender: gender as Gender });
+
+  if (products.length === 0) redirect(`/gender/${gender}`);
+
+  const genderOption: Record<string, string> = {
+    men: "Mens",
+    women: "Womens",
+    kid: "Kids",
+    unisex: "Unisex",
+  };
+  // if (id === "kid") {
+  //   notFound();
+  // }
+  return (
+    <>
+      <Title
+        title={genderOption[gender]}
+        subtitle={"Best Sellers"}
+        className="mb-2"
+      />
+      <ProductGrid products={products} />
+      <Pagination totalPages={totalPages} />
+    </>
+  );
+}
