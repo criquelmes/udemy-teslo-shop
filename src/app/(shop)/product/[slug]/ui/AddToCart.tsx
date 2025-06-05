35d7d8049605ab4ea1f7b912a1e0f7d248b8cce5
Christopher Riquelme <ccriquelmes@gmail.com>
2025-06-05 17:57:15 -0400

    Mensaje de error si no selecciona una talla

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -9,9 +9,30 @@ interface Props {
 }
 export const AddToCart = ({ product }: Props) => {
   const [size, setSize] = useState<Size | undefined>();
-  //   const [quantity, setQuantity] = useState<number>(1);
+  const [quantity, setQuantity] = useState<number>(1);
+  const [posted, setPosted] = useState(false);
+
+  const addToCart = () => {
+    setPosted(true);
+    if (!size) return;
+    console.log({ size, quantity });
+  };
   return (
     <>
+      <div
+        className={`
+          overflow-hidden transition-all duration-400 ease-in-out
+          ${
+            posted && !size
+              ? "max-h-6 opacity-100 mb-2"
+              : "max-h-0 opacity-0 mb-0"
+          }
+        `}
+      >
+        <span className="text-red-600 font-light text-xs block">
+          Please select a size
+        </span>
+      </div>
       {/* Selector de tallas */}
       <SizeSelector
         selectedSize={size}
@@ -20,10 +41,10 @@ export const AddToCart = ({ product }: Props) => {
       />
 
       {/* Selector de cantidad */}
-      <QuantitySelector quantity={2} />
+      <QuantitySelector quantity={quantity} onQuantitySelected={setQuantity} />
 
       {/* Button */}
-      <button className="btn-primary my-5 w-full rounded">
+      <button className="btn-primary my-5 w-full rounded" onClick={addToCart}>
         Agregar al carrito
       </button>
     </>
--- src/components/product/quantity-selector/QuantitySelector.tsx
+++ src/components/product/quantity-selector/QuantitySelector.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import { useState } from "react";
 import { IoAddCircleOutline, IoRemoveCircleOutline } from "react-icons/io5";
 
 interface Props {
   quantity: number;
+  onQuantitySelected?: (value: number) => void;
 }
 
-export const QuantitySelector = ({ quantity }: Props) => {
-  const [count, setCount] = useState(quantity);
-
+export const QuantitySelector = ({ quantity, onQuantitySelected }: Props) => {
   const handleQuantityChange = (value: number) => {
-    if (count + value < 1) return;
-    setCount(count + value);
+    if (quantity + value < 1) return;
+    onQuantitySelected?.(quantity + value);
   };
   return (
     <div className="flex ">
@@ -20,7 +18,7 @@ export const QuantitySelector = ({ quantity }: Props) => {
         <IoRemoveCircleOutline className="text-2xl text-gray-500 hover:text-gray-700" />
       </button>
       <span className="w-20 mx-3 px-5 bg-gray-100 text-center rounded">
-        {count}
+        {quantity}
       </span>
       <button onClick={() => handleQuantityChange(1)}>
         <IoAddCircleOutline className="text-2xl text-gray-500 hover:text-gray-700" />
