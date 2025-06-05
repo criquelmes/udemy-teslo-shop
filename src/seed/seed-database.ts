a812dfc5ec6421e3f6b89961808808a35d80f482
Christopher Riquelme <ccriquelmes@gmail.com>
2025-06-04 22:38:23 -0400

    Semilla de datos - Categorias

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -16,7 +16,15 @@ async function main() {
     data: categoriesData,
   });
 
-  console.log(categoriesData);
+  const categoriesDB = await prisma.category.findMany();
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>);
+
+  console.log("Categories map:", categoriesMap);
+  // console.log("Categories created:", categoriesDB);
+  // console.log(categoriesData);
   console.log("seed executed");
 }
 
