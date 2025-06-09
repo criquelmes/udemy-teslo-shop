2961be77f5ec962c6e37628aa9bb5b7ee76d00a2
Christopher Riquelme <ccriquelmes@gmail.com>
2025-06-09 15:21:55 -0400

    Transaccion- actualizar inventario

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -3,7 +3,6 @@
 import { auth } from "@/auth.config";
 import type { Address, Size } from "@/interfaces";
 import prisma from "@/lib/prisma";
-import { size } from "zod/v4";
 
 interface ProductToOrder {
   productId: string;
@@ -57,43 +56,92 @@ export const placeOrder = async (
   );
 
   // Crear la transaccion de la orden
-  const prismaTx = await prisma.$transaction(async (tx) => {
-    //1. Actualizar el stock de los productos
-    //2. Crear la orden - Encabezado - Detalles
-    const order = await tx.order.create({
-      data: {
-        userId: userId,
-        itemsInOrder: itemsInOrder,
-        subtotal: subTotal,
-        tax: tax,
-        total: total,
+  try {
+    const prismaTx = await prisma.$transaction(async (tx) => {
+      //1. Actualizar el stock de los productos
+      const updatedProductsPromises = products.map((product) => {
+        //Acumular los valores
+        const productQuantity = productsIds
+          .filter((p) => p.productId === product.id)
+          .reduce((acc, item) => item.quantity + acc, 0);
 
-        OrderItem: {
-          createMany: {
-            data: productsIds.map((p) => ({
-              quantity: p.quantity,
-              size: p.size,
-              productId: p.productId,
-              price:
-                products.find((product) => product.id === p.productId)?.price ||
-                0,
-            })),
+        if (productQuantity === 0)
+          throw new Error(`${product.id} does not have quantity - 500`);
+
+        return tx.product.update({
+          where: { id: product.id },
+          data: { inStock: { decrement: productQuantity } },
+        });
+      });
+
+      const updatedProducts = await Promise.all(updatedProductsPromises);
+      // Verificar valores negativos en el stock
+      updatedProducts.forEach((product) => {
+        if (product.inStock < 0) {
+          throw new Error(
+            `Product ${product.title} does not have sufficient stock - 500`
+          );
+        }
+      });
+
+      //2. Crear la orden - Encabezado - Detalles
+      const order = await tx.order.create({
+        data: {
+          userId: userId,
+          itemsInOrder: itemsInOrder,
+          subtotal: subTotal,
+          tax: tax,
+          total: total,
+
+          OrderItem: {
+            createMany: {
+              data: productsIds.map((p) => ({
+                quantity: p.quantity,
+                size: p.size,
+                productId: p.productId,
+                price:
+                  products.find((product) => product.id === p.productId)
+                    ?.price || 0,
+              })),
+            },
           },
         },
-      },
+      });
+      // validar si price es 0 entonces lanzar un error
+      //3. Crear la direccion de envio
+      const { country, ...restAddress } = address;
+      const orderAddress = await tx.orderAddress.create({
+        data: {
+          firstName: restAddress.firstName,
+          lastName: restAddress.lastName,
+          address: restAddress.address,
+          address2: restAddress.address2,
+          postalCode: restAddress.postalCode,
+          city: restAddress.city,
+          phone: restAddress.phone,
+          countryId: country,
+          orderId: order.id,
+        },
+      });
+
+      return {
+        updatedProducts: updatedProducts,
+        order,
+        orderAddress: orderAddress,
+      };
     });
-    // validar si price es 0 entonces lanzar un error
-    //3. Crear la direccion de envio
 
     return {
-      order,
-      updatedProducts: [],
-      orderAddress: {},
+      ok: true,
+      order: prismaTx.order,
+      prismaTx: prismaTx,
     };
-  });
-
-  // return {
-  //   ok: true,
-  //   message: "Order placed successfully",
-  // };
+  } catch (error) {
+    console.error("Error placing order:", error);
+    return {
+      ok: false,
+      message: "Error placing order",
+      error: error instanceof Error ? error.message : "Unknown error",
+    };
+  }
 };
