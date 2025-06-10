cc472aed00c4905c343ce9accab7fd62032019bd
Christopher Riquelme <ccriquelmes@gmail.com>
2025-06-09 23:57:11 -0400

    Skeleton en carga de botones de PayPal

--- src/components/paypal/PayPalButton.tsx
+++ src/components/paypal/PayPalButton.tsx
@@ -1,7 +1,17 @@
 "use client";
 
-import { PayPalButtons } from "@paypal/react-paypal-js";
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
 
 export const PayPalButton = () => {
+  const [{ isPending }] = usePayPalScriptReducer();
+
+  if (isPending) {
+    return (
+      <div className="animate-pulse gap-2 mb-10">
+        <div className="h-12 bg-gray-300 rounded" />
+        <div className="h-12 bg-gray-300 rounded mt-3" />
+      </div>
+    );
+  }
   return <PayPalButtons />;
 };
